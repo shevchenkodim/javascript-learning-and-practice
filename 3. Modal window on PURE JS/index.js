0ce9c687c5c4a5a68e3cd0d59740a360e6fb8816
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-02 15:29:02 +0300

    Implement removed card from html
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- 3. Modal window on PURE JS/index.js
+++ 3. Modal window on PURE JS/index.js
@@ -1,4 +1,4 @@
-const fruits = [
+let fruits = [
     {id: 1, title: 'Яблоки', price: 20, img: 'https://lh6.googleusercontent.com/proxy/Tkcw1FWm3N07S-AJ2Oa4q9yPri0p5WLv49CfRniz7Ue5HEDE6J2vZMnl6eQPr12-TcnO3eRu7-0Pzr4RBMTrnNAr3lEgAf1CV9mJkC7CSyzYaS9ryT5U3WeOz7AfFxY'},
     {id: 2, title: 'Апельсины', price: 30, img: 'https://ranok.ictv.ua/wp-content/uploads/sites/59/2020/01/03/imgonline-com-ua-Compressed-9a2swo5nTb4lam.jpg'},
     {id: 3, title: 'Манго', price: 40, img: 'https://fxmed.com.ua/wp-content/uploads/2019/10/shutterstock_1040578063.jpg'}
@@ -51,7 +51,8 @@ document.addEventListener('click', event => {
             title: 'Вы уверены?',
             content: `<p>Вы удаляете фрукт: <strong>${fruit.title}</strong></p>`
         }).then(() => {
-            console.log('remove');
+            fruits = fruits.filter(f => f.id !== id)
+            render()
         }).catch(() => {
             console.log('Cancel');
         })
