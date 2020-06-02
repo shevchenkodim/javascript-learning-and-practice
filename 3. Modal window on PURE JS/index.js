400ed22630824b0e88f38f501070aa36e62e8473
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-02 15:15:26 +0300

    Implement confirm modal
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- 3. Modal window on PURE JS/index.js
+++ 3. Modal window on PURE JS/index.js
@@ -11,7 +11,7 @@ const toHTML = fruit => `
           <div class="card-body">
             <h5 class="card-title">${fruit.title}</h5>
             <a href="#" class="btn btn-primary" data-btn="price" data-id="${fruit.id}">Посмотреть цену</a>
-            <a href="#" class="btn btn-danger">Удалить</a>
+            <a href="#" class="btn btn-danger" data-btn="remove" data-id="${fruit.id}">Удалить</a>
           </div>
         </div>
     </div>
@@ -24,23 +24,23 @@ function render() {
 
 render()
 
-const modal = $.modal({
-    title: 'Dmytro Modal',
-    closable: true,
-    content: `
-        <p>Lorem ipsum dolor sit.</p>
-    `,
-    width: '400px',
-    footerButtons: [
-        {text: 'Ok', type: 'primary', handler() {
-            console.log('Primary btn clicked');
-        }},
-        {text: 'Cansel', type: 'danger', handler() {
-            console.log('Danger btn clicked');
-            modal.close()
-        }}
-    ]
-})
+// const modal = $.modal({
+//     title: 'Dmytro Modal',
+//     closable: true,
+//     content: `
+//         <p>Lorem ipsum dolor sit.</p>
+//     `,
+//     width: '400px',
+//     footerButtons: [
+//         {text: 'Ok', type: 'primary', handler() {
+//             console.log('Primary btn clicked');
+//         }},
+//         {text: 'Cansel', type: 'danger', handler() {
+//             console.log('Danger btn clicked');
+//             modal.close()
+//         }}
+//     ]
+// })
 
 const priceModal = $.modal({
     title: 'Цена на Товар',
@@ -57,12 +57,21 @@ document.addEventListener('click', event => {
     event.preventDefault()
     const btnType = event.target.dataset.btn
     const id = +event.target.dataset.id
+    const fruit = fruits.find(f => f.id === id)
 
     if (btnType === 'price') {
-        const fruit = fruits.find(f => f.id === id)
         priceModal.setContent(`
             <p>Цена на ${fruit.title}: <strong>${fruit.price}$</strong></p>
         `)
         priceModal.open()
+    } else if (btnType === 'remove') {
+        $.confirm({
+            title: 'Вы уверены?',
+            content: `<p>Вы удаляете фрукт: <strong>${fruit.title}</strong></p>`
+        }).then(() => {
+            console.log('remove');
+        }).catch(() => {
+            console.log('Cancel');
+        })
     }
 })
