e9dec08d4c335883f65f741d48e71938fbe4a44c
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-03 17:14:09 +0300

    Promises examples
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

---
+++ 4. JavaScriptTutorial/promise.js
@@ -0,0 +1,49 @@
+/* const p = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        console.log('Preparing data...')
+        const backendData = {
+            server: 'aws',
+            port: 2000,
+            status: 'working'
+        }
+        //resolve(backendData)
+        reject(backendData)
+    }, 2000)
+})
+
+p.then(data => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.modified = true
+            resolve(data)
+        }, 2000)
+    })
+
+})
+    .then(clientData => {
+        clientData.fromPromise = true
+        return clientData
+    })
+    .then(data => {
+        console.log('Modified', data)
+    })
+    .catch(err => console.error('Error: ', err))
+    .finally(() => console.log('Finally'))
+ */
+
+const sleep = ms => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), ms)
+    })
+}
+
+// sleep(2000).then(() => console.log('After 2 sec'))
+// sleep(3000).then(() => console.log('After 3 sec'))
+
+Promise.all([sleep(2000), sleep(5000)]).then(() => {
+    console.log('All promises')
+})
+
+Promise.race([sleep(2000), sleep(5000)]).then(() => {
+    console.log('Race promises')
+})
