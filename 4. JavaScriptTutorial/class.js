a16315b9e5afa94eb082bc3e47cd44f8158eb87f
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-03 15:18:15 +0300

    JS classes
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

---
+++ 4. JavaScriptTutorial/class.js
@@ -0,0 +1,46 @@
+class Component {
+    constructor(selector) {
+        this.$el = document.querySelector(selector)
+    }
+
+    hide() {
+        this.$el.style.display = 'none'
+    }
+
+    show() {
+        this.$el.style.display = 'block'
+    }
+}
+
+
+class Box extends Component {
+    constructor(options) {
+        super(options.selector)
+
+        this.$el.style.width = this.$el.style.height = options.size + 'px'
+        this.$el.style.background = options.color
+    }
+}
+
+
+const box1 = new Box({
+    selector: '#box1',
+    size: 100,
+    color: 'red'
+})
+
+
+class Circle extends Box {
+    constructor(options) {
+        super(options)
+
+        this.$el.style.borderRadius = '50%'
+    }
+}
+
+
+const c = new Circle({
+    selector: '#circle',
+    size: 90,
+    color: 'green'
+})
